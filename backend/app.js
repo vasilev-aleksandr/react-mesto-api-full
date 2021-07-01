7a3aec14185ca313c47b85bc220eafdb786d4a32
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-07-01 15:54:22 +0300

    fixed bugs

--- backend/app.js
+++ backend/app.js
@@ -37,10 +37,13 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
+
 app.use((req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   }
   next();
 });
