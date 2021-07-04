e29d97ed868d734adf686f58fddaeb3f9cdd132d
Aleksandr Vasilev <sovietxyouth@gmail.com>
2021-07-04 14:59:59 +0300

    fixed 404 bugs

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Вам необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError('Необходима авторизация');
+    throw new UnauthorizedError('Вам необходима авторизация');
   }
 
   req.user = payload;
